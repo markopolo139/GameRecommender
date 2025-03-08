cbeeb21f85366b0f94788a193349c8c4fed1abda
Marek Seget <marek.seget1@gmail.com>
2025-03-08 10:19:05 +0100

    Initialization of project

---
+++ src/main/java/ms/gamerecommender/GameRecommenderApplication.java
@@ -0,0 +1,13 @@
+package ms.gamerecommender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameRecommenderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameRecommenderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ms/gamerecommender/GameRecommenderApplicationTests.java
@@ -0,0 +1,13 @@
+package ms.gamerecommender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameRecommenderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
