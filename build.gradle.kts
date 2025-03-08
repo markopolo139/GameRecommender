950023698d924c9494411954fb3b9b2fd5612261
Marek Seget <marek.seget1@gmail.com>
2025-03-08 13:34:18 +0100

    Add necessary dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,7 +36,18 @@ dependencies {
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
+    testImplementation("junit:junit:4.13")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    implementation("org.apache.mahout:mahout-math:0.13.0")
+    implementation("org.apache.mahout:mahout-mr:0.13.0")
+
+    implementation("org.apache.commons:commons-lang3:3.17.0")
+    implementation("org.apache.commons:commons-collections4:4.5.0-M3")
+
+    implementation("org.springframework.boot:spring-boot-starter-log4j2")
+
+    implementation("ai.djl:api:0.32.0")
 }
 
 tasks.withType<Test> {
--- src/main/java/ms/gamerecommender/GameRecommenderApplication.java
+++ src/main/java/ms/gamerecommender/GameRecommenderApplication.java
@@ -3,6 +3,8 @@ package ms.gamerecommender;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//TODO: Recommendation algorithm (content-based + collaborative) and AI (neural network)
+
 @SpringBootApplication
 public class GameRecommenderApplication {
 
