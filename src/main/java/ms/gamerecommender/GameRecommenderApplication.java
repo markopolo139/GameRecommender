a326d8e9b2aad44e2eb51212dfb694471d70ebf2
Marek Seget <marek.seget1@gmail.com>
2025-03-12 20:03:28 +0100

    feat: Add things to todo

--- src/main/java/ms/gamerecommender/GameRecommenderApplication.java
+++ src/main/java/ms/gamerecommender/GameRecommenderApplication.java
@@ -1,26 +1,18 @@
 package ms.gamerecommender;
 
-import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.util.stream.Collectors;
-
-//TODO: Recommendation algorithm (content-based + collaborative) and AI (neural network)
-// FOr mahoot data must be in form userID, itemID, score
+//TODO: Recommendation algorithm (content-based + collaborative) [Tests] and AI (neural network)
 // Do 2 AI models, deep network, and neural collaborative
-// For now dataaModel from mahoot will be read from csv file
+// For now dataaModel from mahoot will be read from csv file, csv created during loading of user games via api and then saving in resources
+//  and this file will be updated each time some user makes changes to their owned games or ranking or updates time played,
+//  maybe it will be easier to store score in column of table for games instead of calculating it for all owned games each time new game is added
 // For tests create own csv file and substitate properites file
+//  Create in app config for creating beans for recmoennders
 @SpringBootApplication
-@Log4j2
 public class GameRecommenderApplication {
     public static void main(String[] args) {
-        log.error("TEST");
-        log.info("TEST");
         SpringApplication.run(GameRecommenderApplication.class, args);
     }
 }
