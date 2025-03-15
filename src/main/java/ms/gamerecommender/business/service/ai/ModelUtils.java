adb1064e3520619cef96ec41da0143396a1eec33
Marek Seget <marek.seget1@gmail.com>
2025-03-15 15:57:44 +0100

    feat(ai): extract model utils to more common class

---
+++ src/main/java/ms/gamerecommender/business/service/ai/ModelUtils.java
@@ -0,0 +1,38 @@
+package ms.gamerecommender.business.service.ai;
+
+import ai.djl.Model;
+import ai.djl.ndarray.types.Shape;
+import ai.djl.training.DefaultTrainingConfig;
+import ai.djl.training.EasyTrain;
+import ai.djl.training.Trainer;
+import ai.djl.training.dataset.RandomAccessDataset;
+import ai.djl.training.listener.TrainingListener;
+import ai.djl.training.loss.Loss;
+import ai.djl.training.optimizer.Optimizer;
+import ai.djl.translate.TranslateException;
+import lombok.AccessLevel;
+import lombok.SneakyThrows;
+import lombok.experimental.FieldDefaults;
+import lombok.experimental.UtilityClass;
+import lombok.val;
+
+@UtilityClass
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class ModelUtils {
+    @SneakyThrows
+    public <D extends RandomAccessDataset> void modelTraining(Model model, D dataset, int inputSize, int numberOfEpochs) {
+        val datasetSeparated = dataset.randomSplit(8, 2);
+
+        val config = new DefaultTrainingConfig(Loss.l2Loss())
+                .optOptimizer(Optimizer.adam().build())
+                .addTrainingListeners(TrainingListener.Defaults.logging())
+                .addTrainingListeners(new LossLogger());
+
+        try (Trainer trainer = model.newTrainer(config)) {
+            trainer.initialize(new Shape(1, inputSize));
+            EasyTrain.fit(trainer, numberOfEpochs, datasetSeparated[0], datasetSeparated[1]);
+        } catch (TranslateException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- src/main/java/ms/gamerecommender/business/service/ai/dl/DlModelUtils.java
+++ src/main/java/ms/gamerecommender/business/service/ai/dl/DlModelUtils.java
@@ -1,30 +1,22 @@
 package ms.gamerecommender.business.service.ai.dl;
 
 import ai.djl.Model;
-import ai.djl.inference.Predictor;
 import ai.djl.ndarray.NDList;
-import ai.djl.ndarray.types.Shape;
 import ai.djl.nn.Activation;
 import ai.djl.nn.Blocks;
 import ai.djl.nn.SequentialBlock;
 import ai.djl.nn.core.Linear;
-import ai.djl.training.DefaultTrainingConfig;
-import ai.djl.training.EasyTrain;
-import ai.djl.training.Trainer;
-import ai.djl.training.listener.TrainingListener;
-import ai.djl.training.loss.Loss;
-import ai.djl.training.optimizer.Optimizer;
-import ai.djl.translate.TranslateException;
 import ai.djl.translate.Translator;
 import ai.djl.translate.TranslatorContext;
 import lombok.SneakyThrows;
 import lombok.experimental.UtilityClass;
 import lombok.val;
-import ms.gamerecommender.business.service.ai.LossLogger;
 import ms.gamerecommender.business.value.Game;
 import ms.gamerecommender.business.value.UserProfile;
 
 import java.util.List;
+
+import static ms.gamerecommender.business.service.ai.ModelUtils.*;
 
 @UtilityClass
 public class DlModelUtils {
@@ -40,39 +32,6 @@ public class DlModelUtils {
     }
 
     @SneakyThrows
-    private void modelTraining(Model model, ModelDataset dataset, int inputSize, int numberOfEpochs) {
-        val datasetSeparated = dataset.randomSplit(8, 2);
-
-        val config = new DefaultTrainingConfig(Loss.l2Loss())
-                .optOptimizer(Optimizer.adam().build())
-                .addTrainingListeners(TrainingListener.Defaults.logging())
-                .addTrainingListeners(new LossLogger());
-
-        try (Trainer trainer = model.newTrainer(config)) {
-            trainer.initialize(new Shape(1, inputSize));
-            EasyTrain.fit(trainer, numberOfEpochs, datasetSeparated[0], datasetSeparated[1]);
-        } catch (TranslateException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private float predict(Model model, FeatureVector inputFeature) {
-        try (Predictor<float[], Float> predictor = model.newPredictor(new FeatureTranslator())) {
-            return predictor.predict(inputFeature.getFeatureArray());
-        } catch (TranslateException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private List<Float> batchPredict(Model model, List<FeatureVector> inputFeatures) {
-        val inputArrays = inputFeatures.stream().map(FeatureVector::getFeatureArray).toList();
-        try (Predictor<float[], Float> predictor = model.newPredictor(new FeatureTranslator())) {
-            return predictor.batchPredict(inputArrays);
-        } catch (TranslateException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
     public float trainAndPredict(UserProfile user, Game newGame, int numberOfEpochs) {
         val dataset = createModelDatasetForUser(user);
         val newGameVector = transformToFeatureVector(newGame);
@@ -82,10 +41,13 @@ public class DlModelUtils {
 
             modelTraining(model, dataset, FeatureVector.VECTOR_SIZE, numberOfEpochs);
 
-            return predict(model, newGameVector);
+            val predictor = model.newPredictor(new FeatureTranslator());
+
+            return predictor.predict(newGameVector.getFeatureArray());
         }
     }
 
+    @SneakyThrows
     public List<Float> trainAndBatchPredict(UserProfile user, List<Game> newGames, int numberOfEpochs) {
         val dataset = createModelDatasetForUser(user);
         val newGameVector = transformToFeatureVectorList(newGames);
@@ -95,7 +57,9 @@ public class DlModelUtils {
 
             modelTraining(model, dataset, FeatureVector.VECTOR_SIZE, numberOfEpochs);
 
-            return batchPredict(model, newGameVector);
+            val predictor = model.newPredictor(new FeatureTranslator());
+
+            return predictor.batchPredict(newGameVector.stream().map(FeatureVector::getFeatureArray).toList());
         }
     }
 
