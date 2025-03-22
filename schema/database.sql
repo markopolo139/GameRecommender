415dbc1338637a2843fa7b55f8d50d16c75307a6
Marek Seget <marek.seget1@gmail.com>
2025-03-22 12:55:24 +0100

    feat(schema): create database file

---
+++ schema/database.sql
@@ -0,0 +1,35 @@
+create table user_profiles(
+      user_id int primary key not null auto_increment,
+      username varchar(64) unique not null,
+      password varchar(256) not null
+);
+
+create table games(
+    game_id int primary key not null auto_increment,
+    title varchar(256) unique not null,
+    positiveReviewPercentage float not null,
+    metacriticScore float default 0.0,
+    price float default 0.0
+);
+
+create table game_tags(
+    game_id int primary key not null auto_increment,
+    tag varchar(256) not null,
+    constraint game_to_tags foreign key(game_id) references games(game_id)
+        on delete CASCADE
+        on update CASCADE
+);
+
+create table user_games(
+       user_id int not null,
+       game_id int not null,
+       timePlayed float not null,
+       rating float default 0.0,
+       review enum('positive', 'none', 'negative') default 'none',
+       constraint player_to_user_profile foreign key(user_id) references user_profiles(user_id)
+           on delete CASCADE
+           on update CASCADE,
+       constraint game_to_user_games foreign key(game_id) references games(game_id)
+           on delete CASCADE
+           on update CASCADE
+);
