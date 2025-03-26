3e6c7a14053d11cd66fd5b722f54671ef69d8ed9
Marek Seget <marek.seget1@gmail.com>
2025-03-26 19:03:57 +0100

    feat(AppUtils): add logger for null security context

--- src/main/java/ms/gamerecommender/app/AppUtils.java
+++ src/main/java/ms/gamerecommender/app/AppUtils.java
@@ -1,12 +1,19 @@
 package ms.gamerecommender.app;
 
 import lombok.experimental.UtilityClass;
+import lombok.extern.log4j.Log4j2;
 import ms.gamerecommender.app.entities.SecurityUser;
 import org.springframework.security.core.context.SecurityContextHolder;
 
+@Log4j2
 @UtilityClass
 public class AppUtils {
     public int getUserID() {
-        return ((SecurityUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getId();
+        try {
+            return ((SecurityUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getId();
+        } catch (Exception e) {
+            log.error("User is not logged in, or tries to get to resource not for him");
+            throw e;
+        }
     }
 }
