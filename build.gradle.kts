ba6d1c7e788bd59659304d2e2066bdc368efb6a1
Marek Seget <marek.seget1@gmail.com>
2025-04-02 21:38:16 +0200

    Add dependencies for mapping json responses to classes

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,6 +41,8 @@ dependencies {
     testCompileOnly("org.projectlombok:lombok")
     testAnnotationProcessor("org.projectlombok:lombok")
 
+    implementation("com.fasterxml.jackson.core:jackson-databind")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
     testImplementation("junit:junit:4.13")
